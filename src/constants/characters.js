7132d2387d5d93dce00279f9174464ac9cfca91a
Jerry <mikeynjerry@gmail.com>
2019-04-01 10:58:03 -0400

    Partial Character Descriptions

--- src/constants/characters.js
+++ src/constants/characters.js
@@ -1,282 +1,314 @@
 export const characters = {
-  Sonic: { bigLink: 'b/ba/Sonic_SSBU.png/609px-Sonic_SSBU.png', name: 'Sonic' },
+  Sonic: { bigLink: "b/ba/Sonic_SSBU.png/609px-Sonic_SSBU.png", name: "Sonic" },
   Bayonetta: {
-    bigLink: '7/7c/Bayonetta_SSBU.png/611px-Bayonetta_SSBU.png',
-    name: 'Bayonetta'
+    bigLink: "7/7c/Bayonetta_SSBU.png/611px-Bayonetta_SSBU.png",
+    name: "Bayonetta"
   },
-  Wolf: { bigLink: '8/8a/Wolf_SSBU.png/647px-Wolf_SSBU.png', name: 'Wolf' },
-  Ken: { bigLink: 'f/f6/Ken_SSBU.png/600px-Ken_SSBU.png', name: 'Ken' },
+  Wolf: { bigLink: "8/8a/Wolf_SSBU.png/647px-Wolf_SSBU.png", name: "Wolf" },
+  Ken: { bigLink: "f/f6/Ken_SSBU.png/600px-Ken_SSBU.png", name: "Ken" },
   Jigglypuff: {
-    bigLink: '6/6a/Jigglypuff_SSBU.png/600px-Jigglypuff_SSBU.png',
-    name: 'Jigglypuff'
+    bigLink: "6/6a/Jigglypuff_SSBU.png/600px-Jigglypuff_SSBU.png",
+    name: "Jigglypuff"
   },
-  Falco: { bigLink: '8/80/Falco_SSBU.png/600px-Falco_SSBU.png', name: 'Falco' },
-  'Diddy Kong': {
-    bigLink: 'a/a7/Diddy_Kong_SSBU.png/579px-Diddy_Kong_SSBU.png',
-    name: 'Diddy Kong'
+  Falco: { bigLink: "8/80/Falco_SSBU.png/600px-Falco_SSBU.png", name: "Falco" },
+  "Diddy Kong": {
+    bigLink: "a/a7/Diddy_Kong_SSBU.png/579px-Diddy_Kong_SSBU.png",
+    name: "Diddy Kong"
   },
-  Mario: { bigLink: '4/44/Mario_SSBU.png/600px-Mario_SSBU.png', name: 'Mario' },
-  'Dark Pit': {
-    bigLink: '0/09/Dark_Pit_SSBU.png/568px-Dark_Pit_SSBU.png',
-    name: 'Dark Pit'
+  Mario: {
+    bigLink: "4/44/Mario_SSBU.png/600px-Mario_SSBU.png",
+    name: "Mario",
+    description:
+      "Perhaps the most well known video game character of all time, Mario is a jack of all trades, and a great choice for newer players"
   },
-  Chrom: { bigLink: '5/57/Chrom_SSBU.png/510px-Chrom_SSBU.png', name: 'Chrom' },
-  Yoshi: { bigLink: '8/8d/Yoshi_SSBU.png/600px-Yoshi_SSBU.png', name: 'Yoshi' },
-  'Wii Fit Trainer': {
-    bigLink: 'f/ff/Wii_Fit_Trainer_SSBU.png/600px-Wii_Fit_Trainer_SSBU.png',
-    name: 'Wii Fit Trainer'
+  "Dark Pit": {
+    bigLink: "0/09/Dark_Pit_SSBU.png/568px-Dark_Pit_SSBU.png",
+    name: "Dark Pit"
+  },
+  Chrom: { bigLink: "5/57/Chrom_SSBU.png/510px-Chrom_SSBU.png", name: "Chrom" },
+  Yoshi: { bigLink: "8/8d/Yoshi_SSBU.png/600px-Yoshi_SSBU.png", name: "Yoshi" },
+  "Wii Fit Trainer": {
+    bigLink: "f/ff/Wii_Fit_Trainer_SSBU.png/600px-Wii_Fit_Trainer_SSBU.png",
+    name: "Wii Fit Trainer"
   },
   Richter: {
-    bigLink: 'c/c2/Richter_SSBU.png/600px-Richter_SSBU.png',
-    name: 'Richter'
+    bigLink: "c/c2/Richter_SSBU.png/600px-Richter_SSBU.png",
+    name: "Richter",
+    description:
+      "Much like his ancestor Simon, Richter uses a combination of aces, crosses, and holy water to slay demons and undead in kind"
   },
-  'Donkey Kong': {
-    bigLink: 'c/c9/Donkey_Kong_SSBU.png/600px-Donkey_Kong_SSBU.png',
-    name: 'Donkey Kong'
+  "Donkey Kong": {
+    bigLink: "c/c9/Donkey_Kong_SSBU.png/600px-Donkey_Kong_SSBU.png",
+    name: "Donkey Kong",
+    description:
+      "Always ready to bring it on, and with a love of bananas, Donkey Kong has powerful attacks and range, but isn’t very quick"
   },
-  Ryu: { bigLink: '6/61/Ryu_SSBU.png/600px-Ryu_SSBU.png', name: 'Ryu' },
-  'Mega Man': {
-    bigLink: '4/46/Mega_Man_SSBU.png/600px-Mega_Man_SSBU.png',
-    name: 'Mega Man'
+  Ryu: { bigLink: "6/61/Ryu_SSBU.png/600px-Ryu_SSBU.png", name: "Ryu" },
+  "Mega Man": {
+    bigLink: "4/46/Mega_Man_SSBU.png/600px-Mega_Man_SSBU.png",
+    name: "Mega Man"
   },
-  Luigi: { bigLink: 'b/bb/Luigi_SSBU.png/547px-Luigi_SSBU.png', name: 'Luigi' },
-  Fox: { bigLink: '2/2f/Fox_SSBU.png/600px-Fox_SSBU.png', name: 'Fox' },
-  'King K. Rool': {
-    bigLink: 'b/b6/King_K._Rool_SSBU.png/600px-King_K._Rool_SSBU.png',
-    name: 'King K. Rool'
+  Luigi: { bigLink: "b/bb/Luigi_SSBU.png/547px-Luigi_SSBU.png", name: "Luigi" },
+  Fox: { bigLink: "2/2f/Fox_SSBU.png/600px-Fox_SSBU.png", name: "Fox" },
+  "King K. Rool": {
+    bigLink: "b/b6/King_K._Rool_SSBU.png/600px-King_K._Rool_SSBU.png",
+    name: "King K. Rool",
+    description:
+      "The King of the Kremlings, King K Rool uses ranged weapons and his thick stomach armor to constantly keep foes on their toes"
   },
   Isabelle: {
-    bigLink: '2/2b/Isabelle_SSBU.png/600px-Isabelle_SSBU.png',
-    name: 'Isabelle'
+    bigLink: "2/2b/Isabelle_SSBU.png/600px-Isabelle_SSBU.png",
+    name: "Isabelle",
+    description:
+      "The lovable secretary Isabelle is an unconventional fighter, using a variety of household implements to lay the smackdown on he opponents"
   },
-  'Pokémon Trainer': {
+  "Pokémon Trainer": {
     bigLink:
-      '0/0f/Pok%C3%A9mon_Trainer_SSBU.png/800px-Pok%C3%A9mon_Trainer_SSBU.png',
-    name: 'Pokémon Trainer'
+      "0/0f/Pok%C3%A9mon_Trainer_SSBU.png/800px-Pok%C3%A9mon_Trainer_SSBU.png",
+    name: "Pokémon Trainer"
   },
-  'Toon Link': {
-    bigLink: '5/56/Toon_Link_SSBU.png/569px-Toon_Link_SSBU.png',
-    name: 'Toon Link'
+  "Toon Link": {
+    bigLink: "5/56/Toon_Link_SSBU.png/569px-Toon_Link_SSBU.png",
+    name: "Toon Link"
   },
-  Ness: { bigLink: '8/82/Ness_SSBU.png/600px-Ness_SSBU.png', name: 'Ness' },
+  Ness: { bigLink: "8/82/Ness_SSBU.png/600px-Ness_SSBU.png", name: "Ness" },
   Ridley: {
-    bigLink: '2/27/Ridley_SSBU.png/600px-Ridley_SSBU.png',
-    name: 'Ridley'
+    bigLink: "2/27/Ridley_SSBU.png/600px-Ridley_SSBU.png",
+    name: "Ridley",
+    description:
+      "Leader of the Space Pirates, Ridley is a ruthless combatan who excels at dealing high damage and strong aerial attacks"
   },
-  'Duck Hunt': {
-    bigLink: 'd/d8/Duck_Hunt_SSBU.png/600px-Duck_Hunt_SSBU.png',
-    name: 'Duck Hunt'
+  "Duck Hunt": {
+    bigLink: "d/d8/Duck_Hunt_SSBU.png/600px-Duck_Hunt_SSBU.png",
+    name: "Duck Hunt"
   },
-  'Rosalina & Luma': {
-    bigLink: '1/16/Rosalina_%26_Luma_SSBU.png/600px-Rosalina_%26_Luma_SSBU.png',
-    name: 'Rosalina & Luma'
+  "Rosalina & Luma": {
+    bigLink: "1/16/Rosalina_%26_Luma_SSBU.png/600px-Rosalina_%26_Luma_SSBU.png",
+    name: "Rosalina & Luma"
   },
-  'Little Mac': {
-    bigLink: '5/53/Little_Mac_SSBU.png/600px-Little_Mac_SSBU.png',
-    name: 'Little Mac'
+  "Little Mac": {
+    bigLink: "5/53/Little_Mac_SSBU.png/600px-Little_Mac_SSBU.png",
+    name: "Little Mac"
   },
-  Sheik: { bigLink: '0/00/Sheik_SSBU.png/600px-Sheik_SSBU.png', name: 'Sheik' },
-  'Captain Falcon': {
-    bigLink: 'd/da/Captain_Falcon_SSBU.png/600px-Captain_Falcon_SSBU.png',
-    name: 'Captain Falcon'
+  Sheik: { bigLink: "0/00/Sheik_SSBU.png/600px-Sheik_SSBU.png", name: "Sheik" },
+  "Captain Falcon": {
+    bigLink: "d/da/Captain_Falcon_SSBU.png/600px-Captain_Falcon_SSBU.png",
+    name: "Captain Falcon"
   },
-  Cloud: { bigLink: 'b/b3/Cloud_SSBU.png/600px-Cloud_SSBU.png', name: 'Cloud' },
-  Shulk: { bigLink: '0/0f/Shulk_SSBU.png/600px-Shulk_SSBU.png', name: 'Shulk' },
-  'Young Link': {
-    bigLink: '8/8a/Young_Link_SSBU.png/600px-Young_Link_SSBU.png',
-    name: 'Young Link'
+  Cloud: { bigLink: "b/b3/Cloud_SSBU.png/600px-Cloud_SSBU.png", name: "Cloud" },
+  Shulk: { bigLink: "0/0f/Shulk_SSBU.png/600px-Shulk_SSBU.png", name: "Shulk" },
+  "Young Link": {
+    bigLink: "8/8a/Young_Link_SSBU.png/600px-Young_Link_SSBU.png",
+    name: "Young Link"
   },
-  'Bowser Jr.': {
-    bigLink: '2/2b/Bowser_Jr._SSBU.png/600px-Bowser_Jr._SSBU.png',
-    name: 'Bowser Jr.'
+  "Bowser Jr.": {
+    bigLink: "2/2b/Bowser_Jr._SSBU.png/600px-Bowser_Jr._SSBU.png",
+    name: "Bowser Jr."
   },
   Mewtwo: {
-    bigLink: 'd/de/Mewtwo_SSBU.png/594px-Mewtwo_SSBU.png',
-    name: 'Mewtwo'
+    bigLink: "d/de/Mewtwo_SSBU.png/594px-Mewtwo_SSBU.png",
+    name: "Mewtwo"
   },
-  'Mr. Game & Watch': {
+  "Mr. Game & Watch": {
     bigLink:
-      'c/cb/Mr._Game_%26_Watch_SSBU.png/600px-Mr._Game_%26_Watch_SSBU.png',
-    name: 'Mr. Game & Watch'
+      "c/cb/Mr._Game_%26_Watch_SSBU.png/600px-Mr._Game_%26_Watch_SSBU.png",
+    name: "Mr. Game & Watch"
   },
-  'Mii Fighter': {
-    bigLink: '2/26/Mii_Fighter_SSBU.png/800px-Mii_Fighter_SSBU.png',
-    name: 'Mii Fighter'
+  "Mii Fighter": {
+    bigLink: "2/26/Mii_Fighter_SSBU.png/800px-Mii_Fighter_SSBU.png",
+    name: "Mii Fighter"
   },
   Lucina: {
-    bigLink: 'd/dc/Lucina_SSBU.png/600px-Lucina_SSBU.png',
-    name: 'Lucina'
+    bigLink: "d/dc/Lucina_SSBU.png/600px-Lucina_SSBU.png",
+    name: "Lucina"
   },
-  'R.O.B.': {
-    bigLink: '6/60/R.O.B._SSBU.png/600px-R.O.B._SSBU.png',
-    name: 'R.O.B.'
+  "R.O.B.": {
+    bigLink: "6/60/R.O.B._SSBU.png/600px-R.O.B._SSBU.png",
+    name: "R.O.B."
   },
   Inkling: {
-    bigLink: '2/2e/Inkling_SSBU.png/603px-Inkling_SSBU.png',
-    name: 'Inkling'
+    bigLink: "2/2e/Inkling_SSBU.png/603px-Inkling_SSBU.png",
+    name: "Inkling"
   },
-  'Zero Suit Samus': {
-    bigLink: 'f/f0/Zero_Suit_Samus_SSBU.png/600px-Zero_Suit_Samus_SSBU.png',
-    name: 'Zero Suit Samus'
+  "Zero Suit Samus": {
+    bigLink: "f/f0/Zero_Suit_Samus_SSBU.png/600px-Zero_Suit_Samus_SSBU.png",
+    name: "Zero Suit Samus"
   },
-  Robin: { bigLink: '8/82/Robin_SSBU.png/600px-Robin_SSBU.png', name: 'Robin' },
-  Wario: { bigLink: '0/04/Wario_SSBU.png/600px-Wario_SSBU.png', name: 'Wario' },
+  Robin: { bigLink: "8/82/Robin_SSBU.png/600px-Robin_SSBU.png", name: "Robin" },
+  Wario: { bigLink: "0/04/Wario_SSBU.png/600px-Wario_SSBU.png", name: "Wario" },
   Lucario: {
-    bigLink: '0/08/Lucario_SSBU.png/600px-Lucario_SSBU.png',
-    name: 'Lucario'
+    bigLink: "0/08/Lucario_SSBU.png/600px-Lucario_SSBU.png",
+    name: "Lucario"
   },
-  Pit: { bigLink: '3/38/Pit_SSBU.png/600px-Pit_SSBU.png', name: 'Pit' },
-  Pichu: { bigLink: 'c/c1/Pichu_SSBU.png/600px-Pichu_SSBU.png', name: 'Pichu' },
-  Peach: { bigLink: '7/74/Peach_SSBU.png/531px-Peach_SSBU.png', name: 'Peach' },
-  Link: { bigLink: '8/84/Link_SSBU.png/600px-Link_SSBU.png', name: 'Link' },
+  Pit: { bigLink: "3/38/Pit_SSBU.png/600px-Pit_SSBU.png", name: "Pit" },
+  Pichu: { bigLink: "c/c1/Pichu_SSBU.png/600px-Pichu_SSBU.png", name: "Pichu" },
+  Peach: { bigLink: "7/74/Peach_SSBU.png/531px-Peach_SSBU.png", name: "Peach" },
+  Link: {
+    bigLink: "8/84/Link_SSBU.png/600px-Link_SSBU.png",
+    name: "Link",
+    description:
+      "The Hero of Courage, Link bring his full aresenal of remote bombs, boomerangs, and arrows to the battle, and is great at keeping opponents at bay"
+  },
   Olimar: {
-    bigLink: 'b/b3/Olimar_SSBU.png/600px-Olimar_SSBU.png',
-    name: 'Olimar'
+    bigLink: "b/b3/Olimar_SSBU.png/600px-Olimar_SSBU.png",
+    name: "Olimar"
   },
   Incineroar: {
-    bigLink: 'c/c4/Incineroar_SSBU.png/600px-Incineroar_SSBU.png',
-    name: 'Incineroar'
+    bigLink: "c/c4/Incineroar_SSBU.png/600px-Incineroar_SSBU.png",
+    name: "Incineroar",
+    description:
+      "The Heel Pokemon, Incineroar is every bit the grappler in his wrestling persona as he is on the battlefield"
   },
-  Roy: { bigLink: '9/9d/Roy_SSBU.png/600px-Roy_SSBU.png', name: 'Roy' },
-  'Dr. Mario': {
-    bigLink: '3/3f/Dr._Mario_SSBU.png/600px-Dr._Mario_SSBU.png',
-    name: 'Dr. Mario'
+  Roy: { bigLink: "9/9d/Roy_SSBU.png/600px-Roy_SSBU.png", name: "Roy" },
+  "Dr. Mario": {
+    bigLink: "3/3f/Dr._Mario_SSBU.png/600px-Dr._Mario_SSBU.png",
+    name: "Dr. Mario"
   },
-  Samus: { bigLink: '0/03/Samus_SSBU.png/600px-Samus_SSBU.png', name: 'Samus' },
+  Samus: {
+    bigLink: "0/03/Samus_SSBU.png/600px-Samus_SSBU.png",
+    name: "Samus",
+    description:
+      "Famed Bounty Hunter of the Metroid Prime series, Samus specializes in powerful, long range attacks, and is no slouch in melee either"
+  },
   Villager: {
-    bigLink: 'a/ac/Villager_SSBU.png/600px-Villager_SSBU.png',
-    name: 'Villager'
+    bigLink: "a/ac/Villager_SSBU.png/600px-Villager_SSBU.png",
+    name: "Villager"
   },
-  Zelda: { bigLink: 'c/c8/Zelda_SSBU.png/600px-Zelda_SSBU.png', name: 'Zelda' },
-  'Piranha Plant': {
-    bigLink: 'f/f0/Piranha_Plant_SSBU.png/600px-Piranha_Plant_SSBU.png',
-    name: 'Piranha Plant'
+  Zelda: { bigLink: "c/c8/Zelda_SSBU.png/600px-Zelda_SSBU.png", name: "Zelda" },
+  "Piranha Plant": {
+    bigLink: "f/f0/Piranha_Plant_SSBU.png/600px-Piranha_Plant_SSBU.png",
+    name: "Piranha Plant"
   },
   Ganondorf: {
-    bigLink: '9/91/Ganondorf_SSBU.png/595px-Ganondorf_SSBU.png',
-    name: 'Ganondorf'
+    bigLink: "9/91/Ganondorf_SSBU.png/595px-Ganondorf_SSBU.png",
+    name: "Ganondorf"
   },
-  Marth: { bigLink: 'e/e9/Marth_SSBU.png/600px-Marth_SSBU.png', name: 'Marth' },
-  Simon: { bigLink: '9/95/Simon_SSBU.png/600px-Simon_SSBU.png', name: 'Simon' },
-  Lucas: { bigLink: '8/81/Lucas_SSBU.png/600px-Lucas_SSBU.png', name: 'Lucas' },
+  Marth: { bigLink: "e/e9/Marth_SSBU.png/600px-Marth_SSBU.png", name: "Marth" },
+  Simon: {
+    bigLink: "9/95/Simon_SSBU.png/600px-Simon_SSBU.png",
+    name: "Simon",
+    description:
+      "The original Belmont, and slayer of dracula, Simon’s whip has the longest normal attack reach in the game, making him great at playing keep-away"
+  },
+  Lucas: { bigLink: "8/81/Lucas_SSBU.png/600px-Lucas_SSBU.png", name: "Lucas" },
   Palutena: {
-    bigLink: '6/6b/Palutena_SSBU.png/568px-Palutena_SSBU.png',
-    name: 'Palutena'
+    bigLink: "6/6b/Palutena_SSBU.png/568px-Palutena_SSBU.png",
+    name: "Palutena"
   },
-  'Meta Knight': {
-    bigLink: '0/00/Meta_Knight_SSBU.png/600px-Meta_Knight_SSBU.png',
-    name: 'Meta Knight'
+  "Meta Knight": {
+    bigLink: "0/00/Meta_Knight_SSBU.png/600px-Meta_Knight_SSBU.png",
+    name: "Meta Knight"
   },
-  Ike: { bigLink: '8/86/Ike_SSBU.png/600px-Ike_SSBU.png', name: 'Ike' },
+  Ike: { bigLink: "8/86/Ike_SSBU.png/600px-Ike_SSBU.png", name: "Ike" },
   Corrin: {
-    bigLink: 'c/c4/Corrin_SSBU.png/600px-Corrin_SSBU.png',
-    name: 'Corrin'
+    bigLink: "c/c4/Corrin_SSBU.png/600px-Corrin_SSBU.png",
+    name: "Corrin"
   },
-  'Pac-Man': {
-    bigLink: '0/03/Pac-Man_SSBU.png/600px-Pac-Man_SSBU.png',
-    name: 'Pac-Man'
+  "Pac-Man": {
+    bigLink: "0/03/Pac-Man_SSBU.png/600px-Pac-Man_SSBU.png",
+    name: "Pac-Man"
   },
   Pikachu: {
-    bigLink: '9/93/Pikachu_SSBU.png/600px-Pikachu_SSBU.png',
-    name: 'Pikachu'
+    bigLink: "9/93/Pikachu_SSBU.png/600px-Pikachu_SSBU.png",
+    name: "Pikachu"
   },
-  Kirby: { bigLink: '0/07/Kirby_SSBU.png/600px-Kirby_SSBU.png', name: 'Kirby' },
-  'King Dedede': {
-    bigLink: 'f/f5/King_Dedede_SSBU.png/600px-King_Dedede_SSBU.png',
-    name: 'King Dedede'
+  Kirby: { bigLink: "0/07/Kirby_SSBU.png/600px-Kirby_SSBU.png", name: "Kirby" },
+  "King Dedede": {
+    bigLink: "f/f5/King_Dedede_SSBU.png/600px-King_Dedede_SSBU.png",
+    name: "King Dedede"
   },
   Greninja: {
-    bigLink: 'd/da/Greninja_SSBU.png/600px-Greninja_SSBU.png',
-    name: 'Greninja'
+    bigLink: "d/da/Greninja_SSBU.png/600px-Greninja_SSBU.png",
+    name: "Greninja"
   },
   Bowser: {
-    bigLink: '4/49/Bowser_SSBU.png/600px-Bowser_SSBU.png',
-    name: 'Bowser'
+    bigLink: "4/49/Bowser_SSBU.png/600px-Bowser_SSBU.png",
+    name: "Bowser"
   },
-  Snake: { bigLink: '0/02/Snake_SSBU.png/492px-Snake_SSBU.png', name: 'Snake' },
-  Daisy: { bigLink: '2/21/Daisy_SSBU.png/600px-Daisy_SSBU.png', name: 'Daisy' },
-  'Ice Climbers': {
-    bigLink: '1/12/Ice_Climbers_SSBU.png/600px-Ice_Climbers_SSBU.png',
-    name: 'Ice Climbers'
+  Snake: { bigLink: "0/02/Snake_SSBU.png/492px-Snake_SSBU.png", name: "Snake" },
+  Daisy: { bigLink: "2/21/Daisy_SSBU.png/600px-Daisy_SSBU.png", name: "Daisy" },
+  "Ice Climbers": {
+    bigLink: "1/12/Ice_Climbers_SSBU.png/600px-Ice_Climbers_SSBU.png",
+    name: "Ice Climbers"
   },
-  'Dark Samus': {
-    bigLink: 'a/a6/Dark_Samus_SSBU.png/600px-Dark_Samus_SSBU.png',
-    name: 'Dark Samus'
+  "Dark Samus": {
+    bigLink: "a/a6/Dark_Samus_SSBU.png/600px-Dark_Samus_SSBU.png",
+    name: "Dark Samus"
   }
-}
+};
 
 export const characterList = [
-  'Mario',
-  'Donkey Kong',
-  'Link',
-  'Samus',
-  'Dark Samus',
-  'Yoshi',
-  'Kirby',
-  'Fox',
-  'Pikachu',
-  'Luigi',
-  'Ness',
-  'Captain Falcon',
-  'Jigglypuff',
-  'Peach',
-  'Daisy',
-  'Bowser',
-  'Ice Climbers',
-  'Sheik',
-  'Zelda',
-  'Dr. Mario',
-  'Pichu',
-  'Falco',
-  'Marth',
-  'Lucina',
-  'Young Link',
-  'Ganondorf',
-  'Mewtwo',
-  'Roy',
-  'Chrom',
-  'Mr. Game & Watch',
-  'Meta Knight',
-  'Pit',
-  'Dark Pit',
-  'Zero Suit Samus',
-  'Wario',
-  'Snake',
-  'Ike',
-  'Pokémon Trainer',
-  'Diddy Kong',
-  'Lucas',
-  'Sonic',
-  'King Dedede',
-  'Olimar',
-  'Lucario',
-  'R.O.B.',
-  'Toon Link',
-  'Wolf',
-  'Villager',
-  'Mega Man',
-  'Wii Fit Trainer',
-  'Rosalina & Luma',
-  'Little Mac',
-  'Greninja',
-  'Mii Fighter',
-  'Palutena',
-  'Pac-Man',
-  'Robin',
-  'Shulk',
-  'Bowser Jr.',
-  'Duck Hunt',
-  'Ryu',
-  'Ken',
-  'Cloud',
-  'Corrin',
-  'Bayonetta',
-  'Inkling',
-  'Ridley',
-  'Simon',
-  'Richter',
-  'King K. Rool',
-  'Isabelle',
-  'Incineroar',
-  'Piranha Plant'
-]
+  "Mario",
+  "Donkey Kong",
+  "Link",
+  "Samus",
+  "Dark Samus",
+  "Yoshi",
+  "Kirby",
+  "Fox",
+  "Pikachu",
+  "Luigi",
+  "Ness",
+  "Captain Falcon",
+  "Jigglypuff",
+  "Peach",
+  "Daisy",
+  "Bowser",
+  "Ice Climbers",
+  "Sheik",
+  "Zelda",
+  "Dr. Mario",
+  "Pichu",
+  "Falco",
+  "Marth",
+  "Lucina",
+  "Young Link",
+  "Ganondorf",
+  "Mewtwo",
+  "Roy",
+  "Chrom",
+  "Mr. Game & Watch",
+  "Meta Knight",
+  "Pit",
+  "Dark Pit",
+  "Zero Suit Samus",
+  "Wario",
+  "Snake",
+  "Ike",
+  "Pokémon Trainer",
+  "Diddy Kong",
+  "Lucas",
+  "Sonic",
+  "King Dedede",
+  "Olimar",
+  "Lucario",
+  "R.O.B.",
+  "Toon Link",
+  "Wolf",
+  "Villager",
+  "Mega Man",
+  "Wii Fit Trainer",
+  "Rosalina & Luma",
+  "Little Mac",
+  "Greninja",
+  "Mii Fighter",
+  "Palutena",
+  "Pac-Man",
+  "Robin",
+  "Shulk",
+  "Bowser Jr.",
+  "Duck Hunt",
+  "Ryu",
+  "Ken",
+  "Cloud",
+  "Corrin",
+  "Bayonetta",
+  "Inkling",
+  "Ridley",
+  "Simon",
+  "Richter",
+  "King K. Rool",
+  "Isabelle",
+  "Incineroar",
+  "Piranha Plant"
+];
--- src/routes/Characters/components/CharactersPage/CharactersPage.js
+++ src/routes/Characters/components/CharactersPage/CharactersPage.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Scrollbar from 'react-scrollbars-custom'
-import Card from '@material-ui/core/Card'
-import CardActionArea from '@material-ui/core/CardActionArea'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import Button from '@material-ui/core/Button'
-import Typography from '@material-ui/core/Typography'
-import { Slide } from '@material-ui/core'
+import React from "react";
+import PropTypes from "prop-types";
+import Scrollbar from "react-scrollbars-custom";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Slide } from "@material-ui/core";
 
 const CharacterCard = ({ character, classes, image, selectCharacter }) => (
   <Card className={classes.card}>
@@ -17,36 +17,35 @@ const CharacterCard = ({ character, classes, image, selectCharacter }) => (
         className={classes.media}
         image={image}
         title={character}
-        style={{ pointerEvents: 'none' }}
+        style={{ pointerEvents: "none" }}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="h2">
           {character.name}
         </Typography>
-        <Typography component="p">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
+        <Typography component="p">{character.description}</Typography>
       </CardContent>
     </CardActionArea>
     <CardActions>
       <Button
         size="small"
         color="primary"
-        onClick={() => selectCharacter(character)}>
+        onClick={() => selectCharacter(character)}
+      >
         Select
       </Button>
       <a
         target="_blank"
         rel="noopener noreferrer"
-        href={`https://www.ssbwiki.com/${character.name}_(SSBU)`}>
+        href={`https://www.ssbwiki.com/${character.name}_(SSBU)`}
+      >
         <Button size="small" color="primary">
           Learn More
         </Button>
       </a>
     </CardActions>
   </Card>
-)
+);
 
 export const CharactersPage = ({
   characters,
@@ -58,9 +57,9 @@ export const CharactersPage = ({
     <div className={classes.root}>
       <div className={classes.tiles}>
         {characterList.map((characterName, i) => {
-          const character = characters[characterName]
-          const { bigLink: link } = character
-          const image = `https://www.ssbwiki.com/images/thumb/${link}`
+          const character = characters[characterName];
+          const { bigLink: link } = character;
+          const image = `https://www.ssbwiki.com/images/thumb/${link}`;
           return (
             <Slide
               key={characterName}
@@ -68,7 +67,8 @@ export const CharactersPage = ({
               in
               mountOnEnter
               unmountOnExit
-              timeout={{ enter: 300 + 100 * i }}>
+              timeout={{ enter: 300 + 100 * i }}
+            >
               <CharacterCard
                 character={character}
                 image={image}
@@ -76,16 +76,16 @@ export const CharactersPage = ({
                 selectCharacter={selectCharacter}
               />
             </Slide>
-          )
+          );
         })}
       </div>
     </div>
   </Scrollbar>
-)
+);
 
 CharactersPage.propTypes = {
   classes: PropTypes.object.isRequired, // from enhancer (withStyles)
   characters: PropTypes.object // from enhancer (firestoreConnect + connect)
-}
+};
 
-export default CharactersPage
+export default CharactersPage;

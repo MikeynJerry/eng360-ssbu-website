963e6e88e190516dbc435eadad5ac3725471d13f
3PIV <pprovins@vols.utk.ed>
2019-04-03 10:34:32 -0400

    Homepage Update
    "Thanks Jerry"

--- src/routes/Home/components/HomePage/HomePage.js
+++ src/routes/Home/components/HomePage/HomePage.js
@@ -1,17 +1,31 @@
 import React from 'react'
+import './homepage.css'
+import { Container, Row, Col, Button } from 'reactstrap'
 
 const Home = ({ classes }) => (
-  <div className={classes.root}>
-    <div className={classes.circle} />
-    <div className={classes.col}>
-      <div className={classes.quad1}>Test1</div>
-      <div className={classes.quad3}>Test3</div>
-    </div>
-    <div className={classes.col}>
-      <div className={classes.quad2}>Test2</div>
-      <div className={classes.quad4}>Test4</div>
-    </div>
-  </div>
+  <Container>
+    <Row>
+      <Col md="6" className={'padding-0'}>
+        <div className={[classes.quad1, 'quad'].join(' ')} />
+      </Col>
+      <Col md="6" className={'padding-0'}>
+        <div className={[classes.quad3, 'quad'].join(' ')} />
+      </Col>
+    </Row>
+    <Row>
+      <Col xs={{ size: 2, offset: 5 }} className={'padding-0'}>
+        <Button className={'absbetween'} />
+      </Col>
+    </Row>
+    <Row>
+      <Col md="6" className={'padding-0'}>
+        <div className={[classes.quad2, 'quad'].join(' ')} />
+      </Col>
+      <Col md="6" className={'padding-0'}>
+        <div className={[classes.quad4, 'quad'].join(' ')} />
+      </Col>
+    </Row>
+  </Container>
 )
 
 export default Home
--- src/routes/Home/components/HomePage/HomePage.styles.js
+++ src/routes/Home/components/HomePage/HomePage.styles.js
@@ -14,47 +14,35 @@ export default theme => ({
     position: 'absolute',
     top: 'calc(50vh - 20vh)',
     zIndex: '100',
-    backgroundImage: 'url()'
+    width: '30%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    height: 'auto',
+    backgroundColor: 'black',
+    borderRadius: '100%'
   },
   quad1: {
     backgroundColor: 'red',
-    marginLeft: '0',
-    width: '100%',
-    height: 200,
-    borderColor: 'black',
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderBottomWidth: 2.5,
-    borderRightWidth: 2.5
+    backgroundImage:
+      'url(https://i.kym-cdn.com/photos/images/original/001/316/888/f81.jpeg)',
+    height: 200
   },
   quad2: {
     backgroundColor: 'green',
-    width: '100%',
-    height: 200,
-    borderColor: 'black',
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderLeftWidth: 2.5,
-    borderBottomWidth: 2.5
+    backgroundImage:
+      'url(https://cdn.stockphotosecrets.com/wp-content/uploads/2018/08/hide-the-pain-stockphoto-840x560.jpg)',
+    height: 200
   },
   quad3: {
     backgroundColor: 'blue',
-    width: '100%',
-    height: 200,
-    borderColor: 'black',
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderTopWidth: 2.5,
-    borderRightWidth: 2.5
+    backgroundImage:
+      'url(http://www.apimages.com/Images/Ap_Creative_Stock_Header.jpg)',
+    height: 200
   },
   quad4: {
     backgroundColor: 'yellow',
-    width: '100%',
-    height: 200,
-    borderColor: 'black',
-    borderStyle: 'solid',
-    borderWidth: 5,
-    borderTopWidth: 2.5,
-    borderLeftWidth: 2.5
+    backgroundImage:
+      'url(https://static.boredpanda.com/blog/wp-content/uploads/2017/12/funny-weird-wtf-stock-photos-1-5a39121d2fd9b__700.jpg)',
+    height: 200
   }
 })
